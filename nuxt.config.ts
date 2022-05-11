c46c0c82cd408dd54ebe2ef8a554490e2469487b
malolebrin <malolebrin@gmail.com>
2022-05-11 10:48:54 +0200

    feat: config tailwind css and vite auto imports env variables and resolvers

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,39 @@
 import { defineNuxtConfig } from 'nuxt'
+import { HeadlessUiResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
+
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  buildModules: ['@nuxtjs/tailwindcss'],
+  target: 'static',
+  // ssr: false,
+  vite: {
+    plugins: [
+      Components({
+        resolvers: [
+          HeadlessUiResolver({}),
+          (name) => {
+            if (name.includes('IconSolid')) {
+              const realName = name.split('Solid')[0]
+              return `@heroicons/vue/solid/esm/${realName}.js`
+            }
+            if (name.includes('IconOutline')) {
+              const realName = name.split('Outline')[0]
+              return `@heroicons/vue/outline/esm/${realName}.js`
+            }
+          },
+        ],
+        include: [/\.vue$/, /\.vue\?vue/],
+        extensions: ['vue'],
+      }),
+    ],
+  },
+  runtimeConfig: {
+    // https://v3.nuxtjs.org/guide/features/runtime-config#environment-variables
+    MAPBOX_ACCESS_TOKEN: '',
+    public: {
+      MAPBOX_ACCESS_TOKEN: '', // Or a default value
+    }
+  },
 })
