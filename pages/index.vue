82084ef4dbb2e604c4af850a5cc745275404716e
malolebrin <malolebrin@gmail.com>
2022-05-12 08:59:55 +0200

    feat: base app

---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="flex items-center justify-center text-xl text-red-500">
+  {{ t('test') }}
+</div>
+</template>
+
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+
+const { t } = useI18n()
+</script>
