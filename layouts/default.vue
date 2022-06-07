f87b4c8f5d9111af0e6ed8bfaa265d6b762b2bfe
malolebrin <malolebrin@gmail.com>
2022-06-07 12:33:39 +0200

    fix: error before add vueI18n

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="flex flex-col min-h-screen">
     <!-- HEADER -->
+    layout
     <main class="container">
       <NuxtPage />
     </main>
     <!-- FOOTER -->
   </div>
+
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,12 @@
 <template>
 <div class="flex items-center justify-center text-xl text-red-500">
-  {{ t('test') }}
+  <!-- {{ t('test') }} -->
+  index.vue
 </div>
 </template>
 
-<script setup lang="ts">
+<!-- <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
-</script>
+</script> -->
