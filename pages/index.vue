3f46f623054b159947e4d52b554de11fe80cc032
malolebrin <malolebrin@gmail.com>
2022-06-07 13:48:24 +0200

    feat: test traduction in meta

--- pages/index.vue
+++ pages/index.vue
@@ -9,4 +9,14 @@
 import { useI18n } from 'vue-i18n'
 
 const { t } = useI18n()
+
+useHead({
+  title: t('meta.title'),
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+  charset: 'utf-8',
+  meta: [
+    { name: 'description', content: t('meta.description') },
+  ],
+})
+
 </script>
